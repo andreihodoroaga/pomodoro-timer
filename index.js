17a809f54ef115f182ad79e856b2df95076b8d93
Andrei Hodoroaga <andreihodoroaga36@gmail.com>
2021-09-24 15:18:46 +0300

    Added sounds and tracking work sessions
    Implemented a stats button, along with a total of work sessions being displayed on top of the timer. Also a sound is now played when a timer finishes.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const workBtn = document.getElementById("work-btn")
 const shortBreakBtn = document.getElementById("short-break-btn")
 const longBreakBtn = document.getElementById("long-break-btn")
 const timer = document.getElementById("time-display")
+const workSessionsSpan = document.querySelector(".work-sessions")
+const audio = new Audio('./sounds/bell_school_handheld_ring_x3_gentle.mp3');
 
 // Timer variables   
 let workMins = 25
@@ -40,45 +42,88 @@ const updateCountdown = () => {
     let seconds = time % 60
     seconds = seconds < 10 ? '0' + seconds : seconds
     timer.innerHTML = `${minutes}: ${seconds}`
-    time = time < 0 ? 0 : time
+    
+    if(time <= 0) {
+        clearInterval(currentTimer)
+        time = 0
+        updateStats()
+        audio.play()
+    }
+}
+
+// Stats
+if(!("workSessions" in localStorage)) {
+    localStorage.setItem("workSessions", "0");
+}
+workSessionsSpan.textContent = localStorage.getItem("workSessions");
+
+setInterval(() => {
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var currentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    // console.log(date)
+}, 500)
+
+const updateStats = () => {
+    let nrWorkSessions = localStorage.getItem("workSessions");
+    nrWorkSessions++;
+    localStorage.setItem("workSessions", JSON.stringify(nrWorkSessions));
+    workSessionsSpan.textContent = nrWorkSessions;
 }
 
 // Modal 
 const edit = document.getElementById("edit-btn");
-const modal_container = document.querySelector(".modal-container")
-const close = document.getElementById("close-btn")
+const stats = document.getElementById("stats-btn");
+const editModalContainer = document.querySelector(".edit-modal-container");
+const statsModalContainer = document.querySelector(".stats-modal-container");
+const closeEdit = document.getElementById("close-edit-btn");
+const closeStats = document.getElementById("close-stats-btn");
 
 edit.addEventListener("click", () => {
-    modal_container.classList.add("show");
+    editModalContainer.classList.add("show");
 })
 
-close.addEventListener("click", () => {
-    modal_container.classList.remove("show");
+stats.addEventListener("click", () => {
+    statsModalContainer.classList.add("show");
+})
+
+closeEdit.addEventListener("click", () => {
+    editModalContainer.classList.remove("show");
+})
+
+closeStats.addEventListener("click", () => {
+    statsModalContainer.classList.remove("show");
 })
 
 // Theme
 const container = document.querySelector(".container")
 const timerContainer = document.querySelector(".timer")
+const hexagon = document.querySelector(".hexagon")
 const pdBtns = document.querySelectorAll(".pd-btn") // pomodoro buttons
-const prettyThemeBtn = document.querySelector(".pretty-theme-btn");
-const freshThemeBtn = document.querySelector(".fresh-theme-btn");
-const brightThemeBtn = document.querySelector(".bright-theme-btn");
-
-freshThemeBtn.addEventListener("click", () => {
-    changeTheme("#4cb5f5", "#b3c100", "#34675c", "#fff")
-})
+const prettyThemeBtn = document.querySelector(".pretty-theme-btn")
+const freshThemeBtn = document.querySelector(".fresh-theme-btn")
+const brightThemeBtn = document.querySelector(".bright-theme-btn")
 
 prettyThemeBtn.addEventListener("click", () => {
-    changeTheme("#98dbc6", "#f18d9e", "#e6d72a")
+    changeTheme("#98dbc6", "#f18d9e", "#e6d72a");
+})
+
+freshThemeBtn.addEventListener("click", () => {
+    changeTheme("#4cb5f5", "#b3c100", "#34675c", "#fff");
 })
 
 brightThemeBtn.addEventListener("click", () => {
-    changeTheme( "#ffd959", "#f52549", "#fa6775")
+    changeTheme( "#ffd959", "#f52549", "#fa6775");
 })
 
 // Function used to change themes according to the colors
 const changeTheme = (containerBgColor, timerBgColor, btnBgColor, btnColor="#000") => {
     container.style.backgroundColor = containerBgColor;
+    if(containerBgColor == "#4cb5f5") {
+        hexagon.style.backgroundColor = btnBgColor;
+    } else {
+        hexagon.style.backgroundColor = timerBgColor;
+    }
     timerContainer.style.backgroundColor = timerBgColor;
     pdBtns.forEach(pdBtn => {
         pdBtn.style.backgroundColor = btnBgColor;
@@ -95,7 +140,13 @@ minsSlider.oninput = function() {
     minsSpan.innerHTML = mins;
     workMins = mins;
     shortBreakMins = mins < 50 ? (mins / 5) : 10; // make the short break a maximum of 10 mins
-    longBreakMins = mins < 45 ? (parseInt(mins * 0.6)) : 25; // make the long break a maximum of 25 mins
+    longBreakMins = mins < 45 ? (parseInt(mins * 0.6)) : 24; // make the long break a maximum of 25 mins
     if(mins == 40 || mins == 35)
         longBreakMins = 20;
 }
+
+// var today = new Date();
+// var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+// var currentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// var dateTime = date+' '+time;
+
